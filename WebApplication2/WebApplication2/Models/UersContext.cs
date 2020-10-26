a985c100b365e9693d59de0f4363cf601e3f1cff
= <=>
2020-10-26 22:35:16 +0700

    none

---
+++ WebApplication2/WebApplication2/Models/UersContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication2.Models
+{
+    public partial class UersContext : DbContext
+    {
+        
+
+        public UersContext(DbContextOptions<UersContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.HasKey(e => e.Userid);
+
+                entity.Property(e => e.Userid)
+                    .HasColumnName("userid")
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.Property(e => e.DayOfBirthday).HasColumnName("day_of_birthday");
+
+                entity.Property(e => e.Email)
+                    .HasColumnName("email")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Gender)
+                    .HasColumnName("gender")
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Office)
+                    .HasColumnName("office")
+                    .HasMaxLength(10)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication2.Models
+{
+    public partial class Users
+    {
+        public string Userid { get; set; }
+        public string Name { get; set; }
+        public string Office { get; set; }
+        public string Gender { get; set; }
+        public int? DayOfBirthday { get; set; }
+        public string Email { get; set; }
+    }
+}
